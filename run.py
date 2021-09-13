3fd9b4bd819e6f3cdb1aac3c8ee7d50f1aa05605
NikolaVk <nikola@ique.nl>
2021-09-13 13:34:58 +0000

    Add validation to check for exackly 6 values to check for exackly 6 values provided data

--- run.py
+++ run.py
@@ -16,15 +16,35 @@ sales = SHEET.worksheet('sales')
 
 data = sales.get_all_values()
 
+
 def get_sales_data():
     """
     Get sales figures input from user
     """
     print("Please enter sales data from the last market.")
     print("Data should nbe six numbers, seperated by commas.")
-    print("Example: 10, 20, 30m 40m 50, 60\n")
+    print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts allstring valiues into integerts.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren"t exackly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exackly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
